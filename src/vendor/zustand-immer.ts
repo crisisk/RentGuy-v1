d2c35da032dc318b606452d4fc14f334264557c2
Chris Klokkers <130704324+crisisk@users.noreply.github.com>
2025-10-28 19:08:32 +0100

    feat: implement Zustand stores with immer middleware

--- src/vendor/zustand-immer.ts
+++ src/vendor/zustand-immer.ts
@@ -1,7 +1,12 @@
-import type { StateCreator } from './zustand'
+import type { GetState, SetState, StateCreator } from './zustand'
 
-export function immer<State extends object>(
-  initializer: StateCreator<State>,
-): StateCreator<State> {
+export function immer<
+  State extends object,
+  CustomSetState = SetState<State>,
+  CustomGetState = GetState<State>,
+  StoreApiExt = {},
+>(
+  initializer: StateCreator<State, CustomSetState, CustomGetState, StoreApiExt>,
+): StateCreator<State, CustomSetState, CustomGetState, StoreApiExt> {
   return initializer
 }
